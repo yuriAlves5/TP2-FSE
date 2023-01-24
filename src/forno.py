8fd4f11cf880413228f660091e76fa7666a688b2
Yuri Alves <yurivalerio12@gmail.com>
2023-01-24 05:26:22 -0300

    a

--- src/forno.py
+++ src/forno.py
@@ -80,6 +80,7 @@ class Forno:
 
     def Update_temperatura_ambiente(self):
         self.temperatura_ambiente = self.bme.get_internal_temperature()
+        print(self.temperatura_ambiente)
         self.uart.enviar(self.modbus.envia_comando('envia_tempertura_ambiente', self.temperatura_ambiente))
 
     def Update_temperatura_interna(self):
